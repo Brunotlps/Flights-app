984babafcb5a481007ade94a814764ba3caa14be
bruno <brunoteixlps@gmail.com>
2024-05-22 21:39:47 -0300

    3° commit - adding the login feature

--- flights/views.py
+++ flights/views.py
@@ -13,7 +13,8 @@ def flight(request, flight_id):
     flight = Flight.objects.get(pk=flight_id)
     return render(request, "flights/flight.html", {
         "flight": flight,
-        "passengers": flight.passengers.all()
+        "passengers": flight.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
     })
 
 
@@ -22,6 +23,6 @@ def flight(request, flight_id):
 def book(request, flight_id):
     if request.method == "POST":
         flight = Flight.objects.get(pk=flight_id)
-        passenger = Passenger.objects.get(pk=int(request.POST("passenger")))
+        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
         passenger.flights.add(flight)
-        return HttpResponseRedirect(reverse("flight", agrs=(flight.id)))
+        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
