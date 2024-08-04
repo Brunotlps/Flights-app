fcf6b762beecdd5806cd4fc54b4bcec8c62d5d26
bruno <brunoteixlps@gmail.com>
2024-08-04 15:48:24 -0300

    adding some features for admin page

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from django.urls import reverse 
 from django.shortcuts import render
@@ -7,11 +8,32 @@ from django.shortcuts import render
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
+    return render(request, "users/user.html")
     
 
 
 def login_view(request):
-    return render(request, "users/login.html")
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "users/login.html")
+
+    return render(request, "users/login.html", {
+        "message": "Invalid credentials."
+    })
+
+
 
 def logout_view(request):
-    pass
+    logout(request)
+    return render(request, "users/login.html", {
+        "message": "Logged out."
+    })
+    
